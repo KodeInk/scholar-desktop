09a9a20761d153b549419df16f393050df718e56
Muyinda Rogers <muyinda@awamo.com>
2018-01-17 12:05:54 +0300

    Init UI

---
+++ src/main/java/com/scholar/desktop/Application.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.com.scholar.desktop;
+
+/**
+ *
+ * @author mover
+ */
+public class Application {
+
+    public void main(String[] args) {
+
+    }
+}
