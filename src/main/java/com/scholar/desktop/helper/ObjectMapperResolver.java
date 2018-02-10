34ff5fb30558b67fdc24a6db7f00d28a42e64a8f
Muyinda Rogers <moverr@gmail.com>
2018-02-10 23:10:26 +0300

    Debugging

--- src/main/java/com/scholar/desktop/helper/ObjectMapperResolver.java
+++ src/main/java/com/scholar/desktop/helper/ObjectMapperResolver.java
@@ -3,6 +3,7 @@ package main.java.com.scholar.desktop.helper;
 import static com.fasterxml.jackson.core.JsonParser.Feature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER;
 import static com.fasterxml.jackson.core.JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 
@@ -16,8 +17,12 @@ public class ObjectMapperResolver implements ContextResolver<ObjectMapper> {
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
     public ObjectMapperResolver() {
-        MAPPER.enable(ALLOW_UNQUOTED_CONTROL_CHARS);
-        MAPPER.enable(ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER);
+//        String zs = ALLOW_UNQUOTED_CONTROL_CHARS.toString();
+//        MAPPER.enable(SerializationFeature.USE_EQUALITY_FOR_OBJECT_ID)
+//
+//        MAPPER.enable(null);
+//
+//        MAPPER.enable(ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER);
     }
 
     @Override
