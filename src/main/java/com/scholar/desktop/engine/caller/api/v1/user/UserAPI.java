72bca9a00f7fc4e7cb36b7cb60cc4dbaee4c7b61
Muyinda Rogers <moverr@gmail.com>
2018-02-14 07:19:23 +0300

    Helper Enums included

--- src/main/java/com/scholar/desktop/engine/caller/api/v1/user/UserAPI.java
+++ src/main/java/com/scholar/desktop/engine/caller/api/v1/user/UserAPI.java
@@ -5,7 +5,10 @@
  */
 package main.java.com.scholar.desktop.engine.caller.api.v1.user;
 
+import java.io.IOException;
 import java.util.logging.Logger;
+import main.java.com.scholar.desktop.engine.caller.EngineCaller;
+import main.java.com.scholar.desktop.engine.caller.entities.SchoolData;
 
 /**
  *
@@ -13,5 +16,11 @@ import java.util.logging.Logger;
  */
 public class UserAPI {
     private static final Logger LOG = Logger.getLogger(UserAPI.class.getName());
+
+    public static GetUserResponse create(_Us, SchoolData schoolData, String logId) throws IOException {
+        
+        EngineCaller.post("user/v1/", body, schoolData, responseType, logId)
+        return EngineCaller.get("users/" + userId, tenantdata, GetUserResponse.class, logId);
+    }
 
 }
