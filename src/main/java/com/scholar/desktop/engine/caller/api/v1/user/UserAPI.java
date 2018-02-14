d408b7fec42f57fef273aa5af3483454e0fd61a8
Muyinda Rogers <moverr@gmail.com>
2018-02-14 07:10:11 +0300

    User API

---
+++ src/main/java/com/scholar/desktop/engine/caller/api/v1/user/UserAPI.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.com.scholar.desktop.engine.caller.api.v1.user;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author mover 2/14/2018
+ */
+public class UserAPI {
+    private static final Logger LOG = Logger.getLogger(UserAPI.class.getName());
+
+}
