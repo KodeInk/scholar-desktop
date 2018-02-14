4781ef8d0eafd1bbb0d7d885f03d2c5e4ffc7c99
Muyinda Rogers <moverr@gmail.com>
2018-02-14 07:06:55 +0300

    Users package

--- src/main/java/com/scholar/desktop/engine/caller/api/v1/user/response/RoleResponse.java
+++ src/main/java/com/scholar/desktop/engine/caller/api/v1/user/response/RoleResponse.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main.java.com.scholar.desktop.engine.caller.api.v1.accounts.response;
+package main.java.com.scholar.desktop.engine.caller.api.v1.user.response;
 
 import java.util.Arrays;
 import java.util.Objects;
