4ef554bc797ac16de51ca41011dae72fbd4160bb
Muyinda Rogers <moverr@gmail.com>
2018-03-08 04:52:42 +0300

    Connecting the RoleService to Role API

--- src/main/java/com/scholar/desktop/services/roles/RolesService.java
+++ src/main/java/com/scholar/desktop/services/roles/RolesService.java
@@ -5,9 +5,17 @@
  */
 package main.java.com.scholar.desktop.services.roles;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 import main.java.com.scholar.desktop.config.entities.SchoolData;
+import main.java.com.scholar.desktop.engine.caller.api.v1.role.RoleAPI;
+import main.java.com.scholar.desktop.engine.caller.api.v1.user.response.RoleResponse;
+import main.java.com.scholar.desktop.engine.caller.api.v1.user.response.UserResponse;
 import main.java.com.scholar.desktop.services.abstracts.AbstractService;
+import static main.java.com.scholar.desktop.services.abstracts.Offsets.limit;
+import static main.java.com.scholar.desktop.services.abstracts.Offsets.offset;
 import main.java.com.scholar.desktop.services.users.UsersService;
 
 /**
@@ -19,9 +27,34 @@ public class RolesService extends AbstractService {
     private static final Logger LOG = Logger.getLogger(UsersService.class.getName());
     private final SchoolData schoolData;
     private static RolesService instance;
+    private static RoleAPI roleAPI;
+
+    private List<RoleResponse> list = null;
 
     public RolesService(SchoolData schoolData) {
         this.schoolData = schoolData;
+        roleAPI = new RoleAPI(schoolData);
+    }
+
+    public static RolesService getInstance(SchoolData schoolData) {
+        if (instance == null) {
+            instance = new RolesService(schoolData);
+        }
+        return instance;
+    }
+
+    public List<RoleResponse> list() {
+        if (list != null) {
+            return list;
+        }
+        list = new ArrayList<>();
+
+        RoleResponse[] responses = roleAPI.list(offset, limit);
+        if (responses != null) {
+            list.addAll(Arrays.asList(responses));
+        }
+        IncreaseOffsetLimit();
+        return list;
     }
 
 }
