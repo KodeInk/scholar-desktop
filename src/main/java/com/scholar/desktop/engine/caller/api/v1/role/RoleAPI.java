5b97794fcefb12a21d4d91bbf6007ab1c41c4cc8
Muyinda Rogers <moverr@gmail.com>
2018-03-08 04:48:31 +0300

    Role APi get List Roles v1

--- src/main/java/com/scholar/desktop/engine/caller/api/v1/role/RoleAPI.java
+++ src/main/java/com/scholar/desktop/engine/caller/api/v1/role/RoleAPI.java
@@ -5,9 +5,17 @@
  */
 package main.java.com.scholar.desktop.engine.caller.api.v1.role;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
+import javax.ws.rs.core.Response;
 import main.java.com.scholar.desktop.config.entities.SchoolData;
 import main.java.com.scholar.desktop.engine.caller.EngineCaller;
+import main.java.com.scholar.desktop.engine.caller.api.v1.user.response.RoleResponse;
+import main.java.com.scholar.desktop.engine.caller.api.v1.user.response.UserResponse;
+import static main.java.com.scholar.desktop.helper.Utilities.ShowAlertMessage;
+import static main.java.com.scholar.desktop.helper.Utilities.getLimit;
+import static main.java.com.scholar.desktop.helper.Utilities.getOffset;
 
 /**
  *
@@ -23,6 +31,34 @@ public class RoleAPI {
     public RoleAPI(SchoolData schoolData) {
         this.schoolData = schoolData;
         engineCaller = new EngineCaller(schoolData);
+    }
+
+    public RoleResponse[] list(Integer offset, Integer limit) {
+        offset = getOffset(offset);
+        limit = getLimit(limit);
+
+        Map<String, String> queryParameter = new HashMap<>();
+        queryParameter.put("offset", "" + offset);
+        queryParameter.put("limit", "" + limit);
+
+        Response response = engineCaller.get("roles/v1/", queryParameter);
+
+        switch (response.getStatus()) {
+            case 400:
+                ShowAlertMessage(response);
+                break;
+            case 200:
+                RoleResponse[] roleResponse = response.readEntity(RoleResponse[].class);
+                return roleResponse;
+            case 401:
+                ShowAlertMessage(response);
+                break;
+            default:
+                return null;
+
+        }
+
+        return null;
     }
 
 }
