8b096834e678f080c36117e6a09a9e8c482dc504
Muyinda Rogers <moverr@gmail.com>
2018-03-08 04:12:13 +0300

    init Role API

---
+++ src/main/java/com/scholar/desktop/engine/caller/api/v1/role/RoleAPI.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.com.scholar.desktop.engine.caller.api.v1.role;
+
+/**
+ *
+ * @author mover 3/8/2018
+ */
+public class RoleAPI {
+
+}
