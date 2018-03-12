29ad9426d061fabe5f898e85fd3e475a5c90b993
Muyinda Rogers <moverr@gmail.com>
2018-03-13 01:15:24 +0300

    GradingService :

--- src/main/java/com/scholar/desktop/services/grading/GradingService.java
+++ src/main/java/com/scholar/desktop/services/grading/GradingService.java
@@ -5,10 +5,55 @@
  */
 package main.java.com.scholar.desktop.services.grading;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+import main.java.com.scholar.desktop.config.entities.SchoolData;
+import main.java.com.scholar.desktop.engine.caller.api.v1.grading.GradingAPI;
+import main.java.com.scholar.desktop.engine.caller.api.v1.grading.response.GradingResponse;
+import main.java.com.scholar.desktop.services.abstracts.AbstractService;
+import static main.java.com.scholar.desktop.services.abstracts.Offsets.limit;
+import static main.java.com.scholar.desktop.services.abstracts.Offsets.offset;
+
 /**
  *
- * @author Manny
+ * @author mover 3/13/2018
  */
-public class GradingService {
+public class GradingService extends AbstractService {
+
+    private static final Logger LOG = Logger.getLogger(GradingService.class.getName());
+    private final SchoolData schoolData;
+    private static GradingService instance;
+    GradingAPI gradingAPI;
+    private List<GradingResponse> list = null;
+
+    public GradingService(SchoolData schoolData) {
+        this.schoolData = schoolData;
+        gradingAPI = new GradingAPI(schoolData);
+    }
+
+    public static GradingService getInstance(SchoolData schoolData) {
+        if (instance == null) {
+            instance = new GradingService(schoolData);
+        }
+        return instance;
+    }
+
+    public List<GradingResponse> list() {
+
+        if (list != null) {
+            return list;
+        }
+        list = new ArrayList<>();
+
+        GradingResponse[] responses = gradingAPI.list(offset, limit);
+        if (responses != null) {
+            list.addAll(Arrays.asList(responses));
+        }
+        IncreaseOffsetLimit();
+        return list;
+    }
+
 
 }
