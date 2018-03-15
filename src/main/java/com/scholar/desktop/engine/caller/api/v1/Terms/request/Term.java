8e6c28d4bc04e14d3fd524d0870a72d89126c976
Muyinda Rogers <moverr@gmail.com>
2018-03-15 21:30:03 +0300

    Request Response Terms ;

---
+++ src/main/java/com/scholar/desktop/engine/caller/api/v1/Terms/request/Term.java
@@ -0,0 +1,176 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.com.scholar.desktop.engine.caller.api.v1.Terms.request;
+
+import com.codemovers.scholar.engine.helper.enums.StatusEnum;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author mover 12/20/2017
+ */
+public class Term {
+
+    private Integer id;
+    private Integer study_year;
+    private String name;
+    private Date start_date;
+    private Date end_date;
+    private Integer ranking;
+    private StatusEnum status;
+    private Integer author_id;
+    private Date date_created;
+
+    public Term() {
+    }
+
+    public Term(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getStudy_year() {
+        return study_year;
+    }
+
+    public void setStudy_year(Integer study_year) {
+        this.study_year = study_year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+
+    public Integer getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(Integer ranking) {
+        this.ranking = ranking;
+    }
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
+
+    public Integer getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(Integer author_id) {
+        this.author_id = author_id;
+    }
+
+    public Date getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Date date_created) {
+        this.date_created = date_created;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.id);
+        hash = 89 * hash + Objects.hashCode(this.study_year);
+        hash = 89 * hash + Objects.hashCode(this.name);
+        hash = 89 * hash + Objects.hashCode(this.start_date);
+        hash = 89 * hash + Objects.hashCode(this.end_date);
+        hash = 89 * hash + Objects.hashCode(this.ranking);
+        hash = 89 * hash + Objects.hashCode(this.status);
+        hash = 89 * hash + Objects.hashCode(this.author_id);
+        hash = 89 * hash + Objects.hashCode(this.date_created);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Term other = (Term) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.study_year, other.study_year)) {
+            return false;
+        }
+        if (!Objects.equals(this.start_date, other.start_date)) {
+            return false;
+        }
+        if (!Objects.equals(this.end_date, other.end_date)) {
+            return false;
+        }
+        if (!Objects.equals(this.ranking, other.ranking)) {
+            return false;
+        }
+        if (this.status != other.status) {
+            return false;
+        }
+        if (!Objects.equals(this.author_id, other.author_id)) {
+            return false;
+        }
+        return Objects.equals(this.date_created, other.date_created);
+    }
+
+    @Override
+    public String toString() {
+        return "_Term{"
+                + "id=" + id
+                + ", study_year=" + study_year
+                + ", name=" + name
+                + ", start_date=" + start_date
+                + ", end_date=" + end_date
+                + ", ranking=" + ranking
+                + ", status=" + status
+                + ", author_id=" + author_id
+                + ", date_created=" + date_created
+                + "}";
+    }
+
+}
---
+++ src/main/java/com/scholar/desktop/engine/caller/api/v1/Terms/response/TermResponse.java
@@ -0,0 +1,176 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.com.scholar.desktop.engine.caller.api.v1.Terms.response;
+
+import com.codemovers.scholar.engine.helper.enums.StatusEnum;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author mover 12/20/2017
+ */
+public class TermResponse {
+
+    private Integer id;
+    private String study_year;
+    private String name;
+    private Date start_date;
+    private Date end_date;
+    private Integer ranking;
+    private StatusEnum status;
+    private String author;
+    private Date date_created;
+
+    public TermResponse() {
+    }
+
+    public TermResponse(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStudy_year() {
+        return study_year;
+    }
+
+    public void setStudy_year(String study_year) {
+        this.study_year = study_year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+
+    public Integer getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(Integer ranking) {
+        this.ranking = ranking;
+    }
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Date date_created) {
+        this.date_created = date_created;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 23 * hash + Objects.hashCode(this.id);
+        hash = 23 * hash + Objects.hashCode(this.study_year);
+        hash = 23 * hash + Objects.hashCode(this.name);
+        hash = 23 * hash + Objects.hashCode(this.start_date);
+        hash = 23 * hash + Objects.hashCode(this.end_date);
+        hash = 23 * hash + Objects.hashCode(this.ranking);
+        hash = 23 * hash + Objects.hashCode(this.status);
+        hash = 23 * hash + Objects.hashCode(this.author);
+        hash = 23 * hash + Objects.hashCode(this.date_created);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TermResponse other = (TermResponse) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.study_year, other.study_year)) {
+            return false;
+        }
+        if (!Objects.equals(this.start_date, other.start_date)) {
+            return false;
+        }
+        if (!Objects.equals(this.end_date, other.end_date)) {
+            return false;
+        }
+        if (!Objects.equals(this.ranking, other.ranking)) {
+            return false;
+        }
+        if (this.status != other.status) {
+            return false;
+        }
+        if (!Objects.equals(this.author, other.author)) {
+            return false;
+        }
+        return Objects.equals(this.date_created, other.date_created);
+    }
+
+    @Override
+    public String toString() {
+        return "TermResponse{"
+                + "id=" + id
+                + ", study_year=" + study_year
+                + ", name=" + name
+                + ", start_date=" + start_date
+                + ", end_date=" + end_date
+                + ", ranking=" + ranking
+                + ", status=" + status
+                + ", author=" + author
+                + ", date_created=" + date_created
+                + "}";
+    }
+
+}
