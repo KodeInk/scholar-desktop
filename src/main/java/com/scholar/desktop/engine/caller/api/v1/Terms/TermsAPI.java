e9b032e6c7e5458933a6126064f29fda01f16526
Muyinda Rogers <moverr@gmail.com>
2018-03-15 21:32:25 +0300

    Terms API :

---
+++ src/main/java/com/scholar/desktop/engine/caller/api/v1/Terms/TermsAPI.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.com.scholar.desktop.engine.caller.api.v1.Terms;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+import javax.ws.rs.core.Response;
+import main.java.com.scholar.desktop.config.entities.SchoolData;
+import main.java.com.scholar.desktop.engine.caller.EngineCaller;
+import main.java.com.scholar.desktop.engine.caller.api.v1.Terms.response.TermResponse;
+import static main.java.com.scholar.desktop.helper.Utilities.ShowAlertMessage;
+import static main.java.com.scholar.desktop.helper.Utilities.getLimit;
+import static main.java.com.scholar.desktop.helper.Utilities.getOffset;
+
+/**
+ *
+ * @author mover 3/15/2018
+ */
+public class TermsAPI {
+
+    private static final Logger LOG = Logger.getLogger(TermsAPI.class.getName());
+    private final SchoolData schoolData;
+    private static TermsAPI instance;
+    private final EngineCaller engineCaller;
+
+    public TermsAPI(SchoolData schoolData) {
+        this.schoolData = schoolData;
+        engineCaller = new EngineCaller(schoolData);
+    }
+
+    public TermResponse[] list(Integer offset, Integer limit) {
+        offset = getOffset(offset);
+        limit = getLimit(limit);
+
+        Map<String, String> queryParameter = new HashMap<>();
+        queryParameter.put("offset", "" + offset);
+        queryParameter.put("limit", "" + limit);
+
+        Response response = engineCaller.get("terms/v1/", queryParameter);
+
+        switch (response.getStatus()) {
+            case 400:
+                ShowAlertMessage(response);
+                break;
+            case 200:
+                TermResponse[] classResponse = response.readEntity(TermResponse[].class);
+                return classResponse;
+            case 401:
+                ShowAlertMessage(response);
+                break;
+            default:
+                return null;
+
+        }
+
+        return null;
+    }
+
+
+}
