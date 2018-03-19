b1d10681439c6be0bd2e2449af7a669e9847486d
Muyinda Rogers <moverr@gmail.com>
2018-03-19 23:31:12 +0300

    : DepartmentService :

--- src/main/java/com/scholar/desktop/services/departments/DepartmentService.java
+++ src/main/java/com/scholar/desktop/services/departments/DepartmentService.java
@@ -5,9 +5,16 @@
  */
 package main.java.com.scholar.desktop.services.departments;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 import main.java.com.scholar.desktop.config.entities.SchoolData;
+import main.java.com.scholar.desktop.engine.caller.api.v1.departments.DepartmentAPI;
+import main.java.com.scholar.desktop.engine.caller.api.v1.departments.response.DepartmentResponse;
 import main.java.com.scholar.desktop.services.abstracts.AbstractService;
+import static main.java.com.scholar.desktop.services.abstracts.Offsets.limit;
+import static main.java.com.scholar.desktop.services.abstracts.Offsets.offset;
 
 /**
  *
@@ -18,10 +25,34 @@ public class DepartmentService extends AbstractService {
     private static final Logger LOG = Logger.getLogger(DepartmentService.class.getName());
     private final SchoolData schoolData;
     private static DepartmentService instance;
+    private List<DepartmentResponse> list = null;
+    DepartmentAPI deparmentAPI;
 
     public DepartmentService(SchoolData schoolData) {
         this.schoolData = schoolData;
+        deparmentAPI = new DepartmentAPI(schoolData);
     }
 
+    public static DepartmentService getInstance(SchoolData schoolData) {
+        if (instance == null) {
+            instance = new DepartmentService(schoolData);
+        }
+        return instance;
+    }
+
+    public List<DepartmentResponse> list() {
+
+        if (list != null) {
+            return list;
+        }
+        list = new ArrayList<>();
+
+        DepartmentResponse[] responses = deparmentAPI.list(offset, limit);
+        if (responses != null) {
+            list.addAll(Arrays.asList(responses));
+        }
+        IncreaseOffsetLimit();
+        return list;
+    }
 
 }
