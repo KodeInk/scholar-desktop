e61d2c80c1d339c801e794ef674c9b13e4850325
Muyinda Rogers <moverr@gmail.com>
2018-03-19 23:10:43 +0300

    DepartmentService ;

--- src/main/java/com/scholar/desktop/services/departments/DepartmentService.java
+++ src/main/java/com/scholar/desktop/services/departments/DepartmentService.java
@@ -5,10 +5,24 @@
  */
 package main.java.com.scholar.desktop.services.departments;
 
+import java.util.logging.Logger;
+import main.java.com.scholar.desktop.config.entities.SchoolData;
+import main.java.com.scholar.desktop.services.abstracts.AbstractService;
+import main.java.com.scholar.desktop.services.classes.ClassesService;
+
 /**
  *
  * @author mover 3/19/2018
  */
-public class DepartmentService {
+public class DepartmentService extends AbstractService {
+
+    private static final Logger LOG = Logger.getLogger(DepartmentService.class.getName());
+    private final SchoolData schoolData;
+    private static DepartmentService instance;
+
+    public DepartmentService(SchoolData schoolData) {
+        this.schoolData = schoolData;
+    }
+
 
 }
