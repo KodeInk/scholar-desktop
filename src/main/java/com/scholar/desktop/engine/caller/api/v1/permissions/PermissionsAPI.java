679d9ae7ad6861ee55ad5fba4db20f471fe4c134
Muyinda Rogers <moverr@gmail.com>
2018-05-19 19:35:29 +0300

    Debugging :

--- src/main/java/com/scholar/desktop/engine/caller/api/v1/permissions/PermissionsAPI.java
+++ src/main/java/com/scholar/desktop/engine/caller/api/v1/permissions/PermissionsAPI.java
@@ -5,10 +5,24 @@
  */
 package main.java.com.scholar.desktop.engine.caller.api.v1.permissions;
 
+import java.util.logging.Logger;
+import main.java.com.scholar.desktop.config.entities.SchoolData;
+import main.java.com.scholar.desktop.engine.caller.EngineCaller;
+
 /**
  *
- * @author Manny
+ * @author mover
  */
 public class PermissionsAPI {
-    
+
+    private static final Logger LOG = Logger.getLogger(PermissionsAPI.class.getName());
+    private final SchoolData schoolData;
+    private static PermissionsAPI instance;
+    private final EngineCaller engineCaller;
+
+    public PermissionsAPI(SchoolData schoolData) {
+        this.schoolData = schoolData;
+        engineCaller = new EngineCaller(schoolData);
+    }
+
 }
