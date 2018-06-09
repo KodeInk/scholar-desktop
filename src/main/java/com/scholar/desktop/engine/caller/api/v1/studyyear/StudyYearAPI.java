b4be8ad8849fe0baeb1dfd47d435cef00ac03baf
Muyinda Rogers <moverr@gmail.com>
2018-06-09 18:47:57 +0300

    Debugging :

--- src/main/java/com/scholar/desktop/engine/caller/api/v1/studyyear/StudyYearAPI.java
+++ src/main/java/com/scholar/desktop/engine/caller/api/v1/studyyear/StudyYearAPI.java
@@ -15,7 +15,6 @@ import main.java.com.scholar.desktop.config.entities.SchoolData;
 import main.java.com.scholar.desktop.engine.caller.EngineCaller;
 import main.java.com.scholar.desktop.engine.caller.api.v1.abstracts.AbstractAPI;
 import main.java.com.scholar.desktop.engine.caller.api.v1.studyyear.response.StudyYearResponse;
-import main.java.com.scholar.desktop.engine.caller.api.v1.user.response.UserResponse;
 import static main.java.com.scholar.desktop.helper.Utilities.ShowAlertMessage;
 import static main.java.com.scholar.desktop.helper.Utilities.getLimit;
 import static main.java.com.scholar.desktop.helper.Utilities.getOffset;
