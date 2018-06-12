5ba4ff2b974e971edceea789613ea588484fa39d
Muyinda Rogers <moverr@gmail.com>
2018-06-13 02:26:07 +0300

    term service :

--- src/main/java/com/scholar/desktop/services/terms/TermsService.java
+++ src/main/java/com/scholar/desktop/services/terms/TermsService.java
@@ -5,13 +5,19 @@
  */
 package main.java.com.scholar.desktop.services.terms;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 import main.java.com.scholar.desktop.config.entities.SchoolData;
 import main.java.com.scholar.desktop.engine.caller.api.v1.Terms.TermsAPI;
+import main.java.com.scholar.desktop.engine.caller.api.v1.Terms.request.Term;
 import main.java.com.scholar.desktop.engine.caller.api.v1.Terms.response.TermResponse;
+import main.java.com.scholar.desktop.engine.caller.api.v1.classes.request._Class;
+import main.java.com.scholar.desktop.engine.caller.api.v1.classes.response.ClassResponse;
 import main.java.com.scholar.desktop.services.abstracts.AbstractService;
 import static main.java.com.scholar.desktop.services.abstracts.Offsets.limit;
 import static main.java.com.scholar.desktop.services.abstracts.Offsets.offset;
@@ -53,6 +59,30 @@ public class TermsService extends AbstractService {
         }
         IncreaseOffsetLimit();
         return list;
+    }
+
+    public TermResponse create(Term term, String logId) throws IOException {
+        if (term != null) {
+            Map classesMap = getTermMap(term);
+            return termsAPI.create(classesMap, logId);
+        }
+        return null;
+    }
+
+    public Map getTermMap(Term term) {
+
+        Map termMap = new HashMap<>();
+        termMap.put("study_year", term.getStudy_year());
+        termMap.put("name", term.getName());
+        termMap.put("start_date", term.getStart_date());
+        termMap.put("end_date", term.getEnd_date());
+        termMap.put("ranking", term.getRanking());
+
+        System.out.println("==================================");
+        System.out.println(termMap);
+        System.out.println("==================================");
+
+        return termMap;
     }
 
 }
