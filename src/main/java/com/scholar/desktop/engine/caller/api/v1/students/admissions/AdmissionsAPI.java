064377351357ae9118836f33bac1fde4faa90492
Muyinda Rogers <moverr@gmail.com>
2018-06-16 10:38:56 +0300

    admissions API  :

--- src/main/java/com/scholar/desktop/engine/caller/api/v1/students/admissions/AdmissionsAPI.java
+++ src/main/java/com/scholar/desktop/engine/caller/api/v1/students/admissions/AdmissionsAPI.java
@@ -5,10 +5,94 @@
  */
 package main.java.com.scholar.desktop.engine.caller.api.v1.students.admissions;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.core.Response;
+import main.java.com.scholar.desktop.config.entities.SchoolData;
+import main.java.com.scholar.desktop.engine.caller.EngineCaller;
+import main.java.com.scholar.desktop.engine.caller.api.v1.abstracts.AbstractAPI;
+import main.java.com.scholar.desktop.engine.caller.api.v1.classes.ClassesAPI;
+import main.java.com.scholar.desktop.engine.caller.api.v1.classes.response.ClassResponse;
+import main.java.com.scholar.desktop.engine.caller.api.v1.students.admissions.response.StudentAdmissionResponse;
+import static main.java.com.scholar.desktop.helper.Utilities.ShowAlertMessage;
+import static main.java.com.scholar.desktop.helper.Utilities.getLimit;
+import static main.java.com.scholar.desktop.helper.Utilities.getOffset;
+import main.java.com.scholar.desktop.helper.exceptions.BadRequestException;
+import main.java.com.scholar.desktop.helper.exceptions.Message;
+
 /**
  *
  * @author mover 6/16/2018
  */
-public class AdmissionsAPI {
-    
+public class AdmissionsAPI extends AbstractAPI {
+
+    private static final Logger LOG = Logger.getLogger(ClassesAPI.class.getName());
+    private final SchoolData schoolData;
+    private static ClassesAPI instance;
+    private final EngineCaller engineCaller;
+    private Message message = null;
+
+    public AdmissionsAPI(SchoolData schoolData) {
+        this.schoolData = schoolData;
+        engineCaller = new EngineCaller(schoolData);
+    }
+
+    public StudentAdmissionResponse[] list(Integer offset, Integer limit) {
+        offset = getOffset(offset);
+        limit = getLimit(limit);
+
+        Map<String, String> queryParameter = new HashMap<>();
+        queryParameter.put("offset", "" + offset);
+        queryParameter.put("limit", "" + limit);
+
+        Response response = engineCaller.get("admissions/v1/", queryParameter);
+
+        switch (response.getStatus()) {
+            case 400:
+                ShowAlertMessage(response);
+                break;
+            case 200:
+                StudentAdmissionResponse[] addmissionResponse = response.readEntity(StudentAdmissionResponse[].class);
+                return addmissionResponse;
+            case 401:
+                ShowAlertMessage(response);
+                break;
+            default:
+                return null;
+
+        }
+
+        return null;
+    }
+
+    public StudentAdmissionResponse create(Map body, String logId) throws IOException {
+        LOG.log(Level.INFO, body.toString());
+        Response response = engineCaller.post("admissions/v1/", body, logId);
+
+        switch (response.getStatus()) {
+            case 400:
+                message = getMessage(response);
+                throw new BadRequestException(message.getMessage());
+
+            case 500:
+                message = getMessage(response);
+                throw new BadRequestException(message.getMessage());
+
+            case 200:
+                StudentAdmissionResponse addmissionResponse = response.readEntity(StudentAdmissionResponse.class);
+                return addmissionResponse;
+            case 401:
+                message = getMessage(response);
+                throw new BadRequestException(message.getMessage());
+
+            default:
+                return null;
+
+        }
+
+    }
+
 }
